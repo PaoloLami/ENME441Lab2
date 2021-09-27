e7d96a9c2e5075660398146227a26b7242a82744
Paolo Lami <paololami2000@gmail.com>
2021-09-27 20:50:45 +0000

    Fixed syntax

--- lab2.py
+++ lab2.py
@@ -1,15 +1,15 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 from time import sleep
 p1=4
 p2=17
 p3=27
 in1,in2=23,24
-gpio.setmode(gpio.BCM)
-gpio.setup(p1, GPIO.OUT) 
-gpio.setup(p2, GPIO.OUT) 
-gpio.setup(p3, GPIO.OUT) 
-gpio.setup(in1,gpio.IN,pull_up_down=gpio.PUD_DOWN)
-gpio.setup(in2,gpio.IN,pull_up_down=gpio.PUD_DOWN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(p1, GPIO.OUT) 
+GPIO.setup(p2, GPIO.OUT) 
+GPIO.setup(p3, GPIO.OUT) 
+GPIO.setup(in1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(in2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 try:
   while True:
     GPIO.output(p3, 0)
@@ -17,28 +17,22 @@ try:
     GPIO.output(p3, 1)
     sleep(0.5)
   def myCallback(pin):  
-    if pin == in1:
+    if pin==in1:
       pwm = GPIO.PWM(p1, 100)
-      try:
-        pwm.start(0)
-        while 1:
-          for dc in range(101):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.01)
-      except KeyboardInterrupt:
-        print('\nExiting')
-    if pin == in2:
+      pwm.start(0)
+      while 1:
+        for dc in range(101):
+          pwm.ChangeDutyCycle(dc)
+          sleep(0.01)
+    if pin==in2:
       pwm = GPIO.PWM(p2, 100)
-      try:
-        pwm.start(0)
-        while 1:
-          for dc in range(101):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.01)
-      except KeyboardInterrupt:
-        print('\nExiting')
-  gpio.add_event_detect(in1,gpio.RISING,callback=myCallback,bouncetime=100)
-  gpio.add_event_detect(in2,gpio.RISING,callback=myCallback,bouncetime=100
+      pwm.start(0)
+      while 1:
+        for dc in range(101):
+          pwm.ChangeDutyCycle(dc)
+          sleep(0.01)
+  GPIO.add_event_detect(in1,GPIO.RISING,callback=myCallback,bouncetime=100)
+  GPIO.add_event_detect(in2,GPIO.RISING,callback=myCallback,bouncetime=100)
 except KeyboardInterrupt:
   print('\nExiting')
-gpio.cleanup()
+GPIO.cleanup()
