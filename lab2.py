d809e4462b46e9fc8250f6840c6941d741d0f4fe
Paolo Lami <paololami2000@gmail.com>
2021-09-27 21:29:42 +0000

    test

--- lab2.py
+++ lab2.py
@@ -11,5 +11,29 @@ GPIO.setup(p2, GPIO.OUT)
 GPIO.setup(p3, GPIO.OUT) 
 GPIO.setup(in1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(in2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.output(p1, 1)
-sleep(10)
+while True:
+  GPIO.output(p3, 0)
+  sleep(0.5)
+  GPIO.output(p3, 1)
+  sleep(0.5)
+try:
+  def myCallback(pin):  
+    if pin==in1:
+      pwm = GPIO.PWM(p1, 100)
+      pwm.start(0)
+      while 1:
+        for dc in range(101):
+          pwm.ChangeDutyCycle(dc)
+          sleep(0.01)
+    if pin==in2:
+      pwm = GPIO.PWM(p2, 100)
+      pwm.start(0)
+      while 1:
+        for dc in range(101):
+          pwm.ChangeDutyCycle(dc)
+          sleep(0.01)
+  GPIO.add_event_detect(in1,GPIO.RISING,callback=myCallback,bouncetime=100)
+  GPIO.add_event_detect(in2,GPIO.RISING,callback=myCallback,bouncetime=100)
+except KeyboardInterrupt:
+  print('\nExiting')
+GPIO.cleanup()
