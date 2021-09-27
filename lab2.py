ba57b07d7ba3d839877eaef41fe246c0d8405628
Paolo Lami <paololami2000@gmail.com>
2021-09-27 21:38:54 +0000

    bruh

--- lab2.py
+++ lab2.py
@@ -13,33 +13,34 @@ GPIO.setup(in1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(in2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 while True:
   try:
-    def myCallback(pin):  
-      GPIO.output(p3, 0)
-      sleep(0.5)
-      GPIO.output(p3, 1)
-      sleep(0.5)
-      if pin==in1:
-        pwm = GPIO.PWM(p1, 100)
-        pwm.start(0)
-        while 1:
-          for dc in range(101):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.01)
-            GPIO.output(p3, 0)
-            sleep(0.5)
-            GPIO.output(p3, 1)
-            sleep(0.5)
-      if pin==in2:
-        pwm = GPIO.PWM(p2, 100)
-        pwm.start(0)
-        while 1:
-          for dc in range(101):
-            pwm.ChangeDutyCycle(dc)
-            sleep(0.01)
+    def myCallback(pin):
+      while 1:  
         GPIO.output(p3, 0)
         sleep(0.5)
         GPIO.output(p3, 1)
-        sleep(0.5)   
+        sleep(0.5)
+        if pin==in1:
+          pwm = GPIO.PWM(p1, 100)
+          pwm.start(0)
+          while 1:
+            for dc in range(101):
+              pwm.ChangeDutyCycle(dc)
+              sleep(0.01)
+              GPIO.output(p3, 0)
+              sleep(0.5)
+              GPIO.output(p3, 1)
+              sleep(0.5)
+        if pin==in2:
+          pwm = GPIO.PWM(p2, 100)
+          pwm.start(0)
+          while 1:
+            for dc in range(101):
+              pwm.ChangeDutyCycle(dc)
+              sleep(0.01)
+              GPIO.output(p3, 0)
+              sleep(0.5)
+              GPIO.output(p3, 1)
+              sleep(0.5)   
     GPIO.add_event_detect(in1,GPIO.RISING,callback=myCallback,bouncetime=100)
     GPIO.add_event_detect(in2,GPIO.RISING,callback=myCallback,bouncetime=100)
   except KeyboardInterrupt:
