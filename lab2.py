fb9e8a1e98d2d1e7e3a1fb1f566f842552b1354b
Paolo Lami <paololami2000@gmail.com>
2021-09-28 03:30:28 +0000

    test

--- lab2.py
+++ lab2.py
@@ -12,24 +12,21 @@ GPIO.setup(p3, GPIO.OUT)
 GPIO.setup(in1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(in2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 try:
-  while 1:
+  def myCallback(pin):
+    if pin==in1:
+      GPIO.output(p1,1)
+      sleep(2)
+      GPIO.output(p1,0)
+    if pin==in2:
+      GPIO.output(p2,1)
+      sleep(2)
+      GPIO.output(p2,0)  
+  GPIO.add_event_detect(in2,GPIO.FALLING,callback=myCallback,bouncetime=500)
+  while True:
     GPIO.output(p3, 0)
     sleep(0.5)
     GPIO.output(p3, 1)
     sleep(0.5)
-    def myCallback(pin):
-      if pin==in1:
-        GPIO.output(p1,1)
-        sleep(2)
-        GPIO.output(p1,0)
-      if pin==in2:
-        GPIO.output(p2,1)
-        sleep(2)
-        GPIO.output(p2,0)  
-    GPIO.add_event_detect(in1,GPIO.FALLING,callback=myCallback,bouncetime=500)
-    GPIO.add_event_detect(in2,GPIO.FALLING,callback=myCallback,bouncetime=500)
-    GPIO.remove_event_detect(in1)
-    GPIO.remove_event_detect(in2)
 except KeyboardInterrupt:
   print('\nExiting')
 GPIO.cleanup()
