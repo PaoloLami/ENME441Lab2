b5ad1c4b2a4b8bfff409d01ec7b553b598eb6987
Paolo Lami <paololami2000@gmail.com>
2021-09-28 03:28:06 +0000

    fixed

--- buttontest.py
+++ buttontest.py
@@ -14,14 +14,14 @@ GPIO.setup(in2,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 def myCallback(pin):
   if pin==in1:
     GPIO.output(p1,1)
-    sleep(2)
+    time.sleep(2)
     GPIO.output(p1,0)
   if pin==in2:
     GPIO.output(p2,1)
-    sleep(2)
+    time.sleep(2)
     GPIO.output(p2,0)   
-GPIO.add_event_detect(in1,GPIO.FALLING,callback=myCallback,bouncetime=500)
-GPIO.add_event_detect(in2,GPIO.FALLING,callback=myCallback,bouncetime=500)
+GPIO.add_event_detect(in1,GPIO.RISING,callback=myCallback,bouncetime=500)
+GPIO.add_event_detect(in2,GPIO.RISING,callback=myCallback,bouncetime=500)
 while True:
   print('.', end='')
   time.sleep(0.1)
